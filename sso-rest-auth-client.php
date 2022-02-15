b087925fd4ba94406c05bb780c849d49fd489fde
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2022-02-15 16:00:30 +0100

    CHANGES: CHANGED generell behaviour

--- sso-rest-auth-client.php
+++ sso-rest-auth-client.php
@@ -24,6 +24,30 @@ class SsoRestAuthClient
     public function __construct()
     {
         add_filter('authenticate', array($this, 'check_credentials'), 10, 3);
+        add_filter('registration_errors', array($this, 'remote_user_exists'), 10, 3);
+    }
+
+    public function remote_user_exists(WP_Error $error, $sanitized_user_login, $user_email)
+    {
+        if (!empty($sanitized_user_login) && !empty($user_email)) {
+            $url = 'https://test.rpi-virtuell.de/wp-json/sso/v1/remote_user_exists';
+            $response = wp_remote_post($url, array(
+                'method' => 'POST',
+                'body' => array(
+                    'sanitized_user_login' => $sanitized_user_login,
+                    'user_email' => $user_email
+                )));
+            $response = json_decode(wp_remote_retrieve_body($response));
+            if (is_wp_error($response)) {
+                return $response;
+            } elseif ($response->success) {
+                return new WP_Error('User Exists', 'This User already exists'); //TODO: Hier sollte ggf. null returned werden
+            } else {
+                return $error;
+            }
+        } else {
+            return new WP_Error('Missing Parameters', 'Required Parameters are missing!');
+        }
     }
 
     public function check_credentials($user, $username, $password)
@@ -35,20 +59,23 @@ class SsoRestAuthClient
                 'body' => array(
                     'username' => $username,
                     'password' => $password
-                ),
-            ));
+                )));
 
             $response = json_decode(wp_remote_retrieve_body($response));
             if (!is_wp_error($response)) {
-                if ($response->success)
-                {
+                if ($response->success) {
                     if ($user = get_user_by('login', $username)) {
-                        //wp_set_password($password, $user->ID);
+                        if (is_multisite() && !is_user_member_of_blog($user->ID, get_current_blog_id())) {
+                            add_user_to_blog(get_current_blog_id(), $user->ID,get_option('default_role')) ;
+                        }
                         return $user;
                     } elseif ($user = get_user_by('email', $username)) {
+                        if (is_multisite() && !is_user_member_of_blog($user->ID, get_current_blog_id())) {
+                            add_user_to_blog(get_current_blog_id(), $user->ID,get_option('default_role')) ;
+                        }
                         return $user;
                     } else {
-                        $user_id =  wp_insert_user(array(
+                        $user_id = wp_insert_user(array(
                             'user_login' => $response->profile->user_login,
                             'first_name' => $response->profile->first_name,
                             'last_name' => $response->profile->last_name,
@@ -56,15 +83,14 @@ class SsoRestAuthClient
                             'display_name' => $response->profile->display_name,
                             'user_email' => $response->profile->user_email
                         ));
-                        if (is_wp_error($user_id)){
+                        if (is_wp_error($user_id)) {
                             return $user_id->get_error_message();
-                        }else{
-                            return get_user_by('id',$user_id);
+                        } else {
+                            return get_user_by('id', $user_id);
 
                         }
                     }
-                }
-                else{
+                } else {
                     return new WP_Error('NoResponse', 'No Response from Remote Login Server!');
                 }
 
@@ -77,5 +103,6 @@ class SsoRestAuthClient
         }
     }
 }
+
 new SsoRestAuthClient();
 
